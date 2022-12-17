404b0f57ae3dd794881618f54132e7b0c672fde3
lucyleeow <lucyleeow@users.noreply.github.com>
2022-12-17 02:32:38 +0000

    Update vendored modules.

--- napari/_vendor/darkdetect/_linux_detect.py
+++ napari/_vendor/darkdetect/_linux_detect.py
@@ -5,20 +5,25 @@
 #-----------------------------------------------------------------------------
 
 import subprocess
-import typing
 
 def theme():
-    # Here we just triage to GTK settings for now
     try:
+        #Using the freedesktop specifications for checking dark mode
         out = subprocess.run(
-            ['gsettings', 'get', 'org.gnome.desktop.interface', 'gtk-theme'],
+            ['gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme'],
             capture_output=True)
         stdout = out.stdout.decode()
+        #If not found then trying older gtk-theme method
+        if len(stdout)<1:
+            out = subprocess.run(
+                ['gsettings', 'get', 'org.gnome.desktop.interface', 'gtk-theme'],
+                capture_output=True)
+            stdout = out.stdout.decode()
     except Exception:
         return 'Light'
     # we have a string, now remove start and end quote
     theme = stdout.lower().strip()[1:-1]
-    if theme.endswith('-dark'):
+    if '-dark' in theme.lower():
         return 'Dark'
     else:
         return 'Light'
@@ -29,11 +34,12 @@ def isDark():
 def isLight():
     return theme() == 'Light'
 
-def listener(callback: typing.Callable[[str], None]) -> None:
+# def listener(callback: typing.Callable[[str], None]) -> None:
+def listener(callback):
     with subprocess.Popen(
         ('gsettings', 'monitor', 'org.gnome.desktop.interface', 'gtk-theme'),
         stdout=subprocess.PIPE,
         universal_newlines=True,
     ) as p:
         for line in p.stdout:
-            callback('Dark' if line.strip().removeprefix("gtk-theme: '").removesuffix("'").endswith('-dark') else 'Light')
+            callback('Dark' if '-dark' in line.strip().removeprefix("gtk-theme: '").removesuffix("'").lower() else 'Light')
